0923cbea8fa38bb621335668663ef3a5fb7baaa3
hugosenari <hugosenari@gmail.com>
2022-02-27 18:07:13 -0300

    fix(moe): moe documentation

--- hugosenari.nix
+++ hugosenari.nix
@@ -2,7 +2,7 @@ let
   url = "https://github.com/hugosenari/nixos-config.git";
   src =  builtins.fetchGit {
     inherit url;
-    rev = "f9c6f191191c4af02c118ada411c91de3720fab4";
+    rev = "ac59e5dab2f276cf6826edb520d9db21564626b6";
   };
 in
 {
