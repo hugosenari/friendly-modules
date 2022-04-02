7db1d816843b60237576ee808134614ed58e9a48
hugosenari <hugosenari@gmail.com>
2022-04-02 17:33:59 -0300

    feat: add more hlissenerx modules

--- project.nix
+++ project.nix
@@ -22,12 +22,13 @@
   # enable .gitignore creation
   files.gitignore.enable = true;
   files.gitignore.template."Global/Archives" = true;
-  files.gitignore.template."Global/Backup" = true;
-  files.gitignore.template."Global/Diff" = true;
+  files.gitignore.template."Global/Backup"   = true;
+  files.gitignore.template."Global/Diff"     = true;
   # now we can use 'convco' command https://convco.github.io
   files.cmds.convco = true;
   # now we can use 'featw' command as alias to convco
-  files.alias.feat = ''convco commit --feat $@'';
-  files.alias.fix = ''convco commit --fix $@'';
+  files.alias.feat  = ''convco commit --feat $@'';
+  files.alias.fix   = ''convco commit --fix $@'';
   files.alias.chore = ''convco commit --chore $@'';
+  files.alias.serve = ''cd $PRJ_ROOT/gh-pages;mdbook serve'';
 }
