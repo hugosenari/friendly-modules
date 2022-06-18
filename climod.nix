c4074ba386232dbf6ba114de4152dfa441ec210a
hugosenari <hugosenari@gmail.com>
2022-06-18 04:06:52 -0300

    feat(climod): add climod documentation

--- climod.nix
+++ climod.nix
@@ -1,3 +1,4 @@
+{ pkgs, ...}:
 let
   url = "https://github.com/nixosbrasil/climod";
   src =  builtins.fetchGit {
@@ -5,10 +6,17 @@ let
     rev = "8c15c340dab1074f87cdcf1daa87b65b732c4a19";
     ref = "master";
   };
+  pkg = pkgs.writeText "climodmod.nix"
+    (builtins.replaceStrings
+      ["        options = command;"]
+      [''options.name = mkOption { type = str; default = "..."; description = "This is a recursive from command"; };'']
+      (builtins.readFile "${src}/modules/common.nix"));
 in
 {
   files.docs."/gh-pages/src/climod.md".modules = [
-    "${src}/modules/default.nix"
+    "${pkg}"
+    "${src}/modules/extra.nix"
+    "${src}/modules/bash/default.nix"
   ];
   files.mdbook.summary = ''
     ---
